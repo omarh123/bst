5364abc6a357ee38f9b07df97475d709829f6c43
AnhQuach <lananh16392@gmail.com>
2013-04-27 13:44:15 -0300

    Update BST.h

--- BST.h
+++ BST.h
@@ -1,27 +1,24 @@
-//
-//  BST.h
-//  BST
-//
-//  Created by omar hashmi on 4/25/13.
-//  Copyright (c) 2013 edu.stanford.cs193p.Omar. All rights reserved.
-//
-
 #ifndef __BST__BST__
 #define __BST__BST__
 
 #include <iostream>
 #include "BinNode.h"
+
 using namespace std;
+
 class BST{
-public:
-    void insertNode(BinNode *anotherNode);
-    string inorderWalk(int i);
-    string  preOrderWalk(int i);
-    BinNode getRoot();
-private:
-    BinNode* root;
+
+    public:
+		BST();
+    	void insertNode(BinNode *anotherNode);
+    	string inorderWalk(int i);
+		string  preOrderWalk(int i);
+    	BinNode getRoot();
+
+		~BST();
+	private:
+    	BinNode* root;
     
 };
 
 #endif /* defined(__BST__BST__) */
-
