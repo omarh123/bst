1a8a5afe71e55339e5b4bbc3cc2df5f32263a781
AnhQuach <lananh16392@gmail.com>
2013-04-27 15:06:14 -0300

    Update BinNode.h

--- BinNode.h
+++ BinNode.h
@@ -1,36 +1,28 @@
-//
-//  BinNode.h
-//  BST
-//
-//  Created by omar hashmi on 4/25/13.
-//  Copyright (c) 2013 edu.stanford.cs193p.Omar. All rights reserved.
-//
-
 #ifndef __BST__BinNode__
 #define __BST__BinNode__
 
 #include <iostream>
+
 using namespace std;
+
 class BinNode{
 
-private:
+    private:
+		BinNode *left;
+		BinNode *right;
+		string data;
 
-BinNode *left;
-BinNode *right;
-string data;
-
-public:
-    BinNode();
-    BinNode(string Data);
-    BinNode* getLeft();
-    BinNode* getRight();
-    string getData();
-    void setLeft(BinNode *LeftIn);
-    void setRight(BinNode *RightIn);
-
-    ~BinNode();
-
-
+	public:
+		BinNode();
+		BinNode(string Data);
+		BinNode* getLeft();
+		BinNode* getRight();
+		string getData();
+		void setLeft(BinNode *LeftIn);
+		void setRight(BinNode *RightIn);
+		
+		~BinNode();
 
 };
+
 #endif /* defined(__BST__BinNode__) */
